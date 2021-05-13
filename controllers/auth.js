5013bf7801b62d1db3eb36c0dc8028194a0ee6a9
CarlosGRJ <carlosgrj@outlook.com>
2021-05-13 11:41:16 -0500

    API User Response

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,33 @@
-exports.createOrUpdateUser = (req, res) => {
-   res.json({
-      data: 'Hey you hit create-or-update-user API endpoint',
+const User = require('../models/user');
+
+exports.createOrUpdateUser = async (req, res) => {
+   const { name, picture, email } = req.user;
+
+   const user = await User.findOneAndUpdate(
+      { email },
+      { name: email.split('@')[0], picture },
+      { new: true },
+   );
+
+   if (user) {
+      console.log('USER UPDATED', user);
+      res.json(user);
+   } else {
+      const newUser = await new User({
+         email,
+         name: email.split('@')[0],
+         picture,
+      }).save();
+      console.log('USER CREATED', newUser);
+      res.json(newUser);
+   }
+};
+
+exports.currentUser = async (req, res) => {
+   User.findOne({ email: req.user.email }).exec((err, user) => {
+      if (err) {
+         throw new Error(err);
+      }
+      res.json({ user });
    });
 };
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,16 @@
 const admin = require('../firebase/index');
 
-exports.authCheck = (req, res, next) => {
-   console.log(req.headers); // token
-   next();
+exports.authCheck = async (req, res, next) => {
+   try {
+      const firebaseUser = await admin
+         .auth()
+         .verifyIdToken(req.headers.authtoken);
+      console.log('FIREBASE USER IN AUTHCHECK ', firebaseUser);
+      req.user = firebaseUser;
+      next();
+   } catch (error) {
+      res.status(401).json({
+         error: 'Invalid or expired token',
+      });
+   }
 };
