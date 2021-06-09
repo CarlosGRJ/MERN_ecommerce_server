27ceb7345bf4b70061c766bd64dbb0acc3452a55
CarlosGRJ <carlosgrj@outlook.com>
2021-06-09 13:44:33 -0500

    Product Update response error

--- controllers/product.js
+++ controllers/product.js
@@ -59,6 +59,9 @@ exports.update = async (req, res) => {
       res.json(updated);
    } catch (error) {
       console.log('PRODUCT UPDATE ERROR ----> ', error);
-      return res.status(400).send('Product update failed');
+      // return res.status(400).send('Product update failed');
+      res.status(400).json({
+         err: err.message,
+      });
    }
 };
