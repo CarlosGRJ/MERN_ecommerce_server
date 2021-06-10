b2d8ac7386255fdffbc2128f170ae6c3058aae7a
CarlosGRJ <carlosgrj@outlook.com>
2021-06-10 09:02:28 -0500

    List Product with Sort Order and Limit

--- controllers/product.js
+++ controllers/product.js
@@ -65,3 +65,20 @@ exports.update = async (req, res) => {
       });
    }
 };
+
+exports.list = async (req, res) => {
+   try {
+      // createdAt/updatedAt, desc/asc
+      const { sort, order, limit } = req.body;
+      const products = await Product.find({})
+         .populate('category')
+         .populate('subs')
+         .sort([sort, order])
+         .limit(limit)
+         .exec();
+
+      res.json(products);
+   } catch (error) {
+      console.log(error);
+   }
+};
