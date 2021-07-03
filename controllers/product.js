a44c584b1328092e5ae601df2cdc9d897febf0d9
CarlosGRJ <carlosgrj@outlook.com>
2021-07-03 11:51:55 -0500

    Filter By Star Rating

--- controllers/product.js
+++ controllers/product.js
@@ -210,8 +210,39 @@ const handleCategory = async (req, res, category) => {
    }
 };
 
+const handleStar = (req, res, stars) => {
+   Product.aggregate([
+      {
+         $project: {
+            document: '$$ROOT',
+            // title: "$title"
+            floorAverage: {
+               $floor: { $avg: '$ratings.star' }, // 3.33 floor to 3
+            },
+         },
+      },
+      {
+         $match: { floorAverage: stars },
+      },
+   ])
+      .limit(12)
+      .exec((err, aggregates) => {
+         if (err) console.log('AGGREGATE ERROR', err);
+
+         console.log('aggregates -> ', aggregates);
+         Product.find({ _id: aggregates })
+            .populate('category', '_id name')
+            .populate('subs', '_id name')
+            .populate('ratings.postedBy', '_id name')
+            .exec((err, products) => {
+               if (err) console.log('PRODUCT AGGREGATE ERROR', err);
+               res.json(products);
+            });
+      });
+};
+
 exports.searchFilters = async (req, res) => {
-   const { query, price, category } = req.body;
+   const { query, price, category, stars } = req.body;
 
    if (query) {
       console.log('query --->', query);
@@ -227,5 +258,10 @@ exports.searchFilters = async (req, res) => {
    if (category) {
       console.log('category ---> ', category);
       await handleCategory(req, res, category);
+   }
+
+   if (stars) {
+      console.log('stars ---> ', stars);
+      await handleStar(req, res, stars);
    }
 };
