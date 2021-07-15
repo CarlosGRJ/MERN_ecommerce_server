733630a50b8970c703663737c37a78224569d4fb
CarlosGRJ <carlosgrj@outlook.com>
2021-07-15 16:04:11 -0500

    Save Cart To Database

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,17 @@
 const express = require('express');
-const jwt = require('jsonwebtoken');
 const router = express.Router();
+// middlewares
+const { authCheck } = require('../middlewares/auth');
+// controllers
+const { userCart } = require('../controllers/user');
 
-router.get('/user', (req, res) => {
-   res.json({
-      data: 'Hey you hit user API endpoint',
-   });
-});
+
+router.post('/cart', authCheck, userCart);  // save cart
+
+// router.get('/user', (req, res) => {
+//    res.json({
+//       data: 'Hey you hit user API endpoint',
+//    });
+// });
 
 module.exports = router;
