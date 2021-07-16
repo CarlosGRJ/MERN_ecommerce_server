7adc25ba9444e35180f87ab259f8a30045ea83aa
CarlosGRJ <carlosgrj@outlook.com>
2021-07-16 11:23:18 -0500

    Save Address

--- controllers/user.js
+++ controllers/user.js
@@ -25,10 +25,10 @@ exports.userCart = async (req, res) => {
       object.count = cart[i].count;
       object.color = cart[i].color;
       // get price for creating total
-      let { price } = await Product.findById(cart[i]._id)
+      let productFromDb = await Product.findById(cart[i]._id)
          .select('price')
          .exec();
-      object.price = price;
+      object.price = productFromDb.price;
 
       products.push(object);
    }
@@ -63,4 +63,12 @@ exports.emptyCart = async (req, res) => {
    const user = await User.findOne({ email: req.user.email }).exec();
    const cart = await Cart.findOneAndRemove({ orderedBy: user._id }).exec();
    res.json(cart);
+};
+
+exports.saveAddress = async (req, res) => {
+   const userAddress = await User.findOneAndUpdate(
+      { email: req.user.email },
+      { address: req.body.address },
+   ).exec();
+   res.json({ ok: true });
 };
