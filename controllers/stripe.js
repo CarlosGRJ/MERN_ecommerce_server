332f13c339bd65a9ba10d4eb0d7976bae3863241
CarlosGRJ <carlosgrj@outlook.com>
2021-07-20 19:21:18 -0500

    Charging Actual Cart Total

--- controllers/stripe.js
+++ controllers/stripe.js
@@ -8,8 +8,15 @@ exports.createPaymentIntent = async (req, res) => {
    // later apply coupon
    // later calculate price
 
+   // 1 find user
+   const user = await User.findOne({ email: req.user.email }).exec();
+   // 2 get user cart total
+   const { cartTotal } = await Cart.findOne({ orderedBy: user._id }).exec();
+
+   console.log('CART Total Charged', cartTotal);
+   // create payment intent with order amount and currency
    const paymentIntent = await stripe.paymentIntents.create({
-      amount: 100,
+      amount: cartTotal * 100,
       currency: 'usd',
    });
 
