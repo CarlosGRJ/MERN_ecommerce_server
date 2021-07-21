7f75908565851fe2c4621a4120c545d1198d7d85
CarlosGRJ <carlosgrj@outlook.com>
2021-07-21 12:05:20 -0500

    Create Order Endpoint

--- routes/user.js
+++ routes/user.js
@@ -9,12 +9,15 @@ const {
    emptyCart,
    saveAddress,
    applyCouponToUserCart,
+   createOrder,
 } = require('../controllers/user');
 
 router.post('/user/cart', authCheck, userCart); // save cart
 router.get('/user/cart', authCheck, getUserCart);
 router.delete('/user/cart', authCheck, emptyCart);
 router.post('/user/address', authCheck, saveAddress); // save cart
+
+router.post('/user/order', authCheck, createOrder);
 
 // coupon
 router.post('/user/cart/coupon', authCheck, applyCouponToUserCart);
